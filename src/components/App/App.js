6a7e5c5973b275d0308b123af174190cd3b6a6ab
Łukasz Kalemba <lukasz.kalemba1@gmail.com>
2020-01-23 21:07:21 +0100

    Create form

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,7 +1,50 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const App = () => {
-  return <div>App.js</div>;
+  const [formData, setFormData] = useState({
+    name: '',
+    email: '',
+    message: ''
+  });
+
+  const { name, email, message } = formData;
+
+  const handleChange = e => {
+    const { name, value } = e.target;
+
+    setFormData({ ...formData, [name]: value });
+  };
+
+  return (
+    <form name='contact' method='POST' data-netlify='true'>
+      <p>
+        <label>
+          Your Name:{' '}
+          <input type='text' name='name' value={name} onChange={handleChange} />
+        </label>
+      </p>
+      <p>
+        <label>
+          Your Email:{' '}
+          <input
+            type='email'
+            name='email'
+            value={email}
+            onChange={handleChange}
+          />
+        </label>
+      </p>
+      <p>
+        <label>
+          Message:{' '}
+          <textarea name='message' value={message} onChange={handleChange} />
+        </label>
+      </p>
+      <p>
+        <button type='submit'>Send</button>
+      </p>
+    </form>
+  );
 };
 
 export default App;
